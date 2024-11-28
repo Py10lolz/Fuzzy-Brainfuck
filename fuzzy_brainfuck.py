787541b16e4da009b960226b25a63bb0341770f6
Py10lolz <88994127+Py10lolz@users.noreply.github.com>
2024-11-28 20:56:33 +0800

    Update fuzzy_brainfuck.py

--- fuzzy_brainfuck.py
+++ fuzzy_brainfuck.py
@@ -18,28 +18,30 @@ class Fuzzy_Brainfuck:
 		self.program_size = program_size
 		self.output_size = output_size
 		self.memory_size = memory_size
+		self.max_loop_count = max_loop_count
 		if raw_program == None:
-			self.raw_program = tf.uniform(shape = (program_size, 8), minval = -2.0, maxval = 2.0)
+			self.raw_program = tf.random.uniform(shape = (program_size, 8), minval = -2.0, maxval = 2.0)
 		else:
 			self.raw_program = raw_program
 		self.initialize(inp)
 
-	def initialize(self, inp, program_modified = True):
+	def initialize(self, inp, output_size = None, program_modified = True):
 		# PROGRAM
-		if program_modified: self.program = tf.nn.softmax(self.raw_program)
-		self.halt_point = tf.Variable([0.0]*(program_size-1) + [1.0], trainable = False) # marks the very last instruction.
+		if program_modified: self.program = tf.Variable(tf.nn.softmax(self.raw_program))
+		self.halt_point = tf.Variable([0.0]*(self.program_size-1) + [1.0], trainable = False) # marks the very last instruction.
 		self.direction = tf.Variable(1.0, trainable = False) # direction of execution. A value of 1 corresponds to shifting the program forward and a value of 0 corresponds to shifting it backwards.
 		self.halt = tf.Variable(0.0, trainable = False) # tendency to ignore instruction (simulating halting). Value of 1 completely ignores instructions, while a value of 0 allows instructions to be executed in full strength.
-		self.loop_counter = tf.Variable([1.0]+[0.0]*max_loop_count, trainable = False) # counts loops (in fuzzy manner). A non-zero value causes the muting of instructions because we are searching for the matching brace.
+		self.loop_counter = tf.Variable([1.0]+[0.0]*self.max_loop_count, trainable = False) # counts loops (in fuzzy manner). A non-zero value causes the muting of instructions because we are searching for the matching brace.
 		# INPUT
 		if inp == None:
 			self.input = tf.Variable([[1.0]+[0.0]*255], trainable = False)
 		else:
 			self.input = inp
 		# OUTPUT
-		self.output = tf.Variable([[1.0]+[0.0]*255]*output_size, trainable = False)
+		if output_size != None: self.output_size = output_size
+		self.output = tf.Variable([[1.0]+[0.0]*255]*self.output_size, trainable = False)
 		# MEMORY
-		self.memory = tf.Variable([[1.0]+[0.0]*255]*memory_size, trainable = False)
+		self.memory = tf.Variable([[1.0]+[0.0]*255]*self.memory_size, trainable = False)
 		self.time_counter = 0
 
 	def forward(self):
@@ -54,12 +56,12 @@ class Fuzzy_Brainfuck:
 		# Input
 		I = self.program[0, INP] * (1 - self.halt) * (1 - self.loop_counter[0])
 		shifted_input = tf.roll(self.input, shift = 1, axis = 0)
-		self.input = I * shifted_input + (1 - I) * shifted_input
+		self.input = I * shifted_input + (1 - I) * self.input
 		# Output
 		O = self.program[0, OUT] * (1 - self.halt) * (1 - self.loop_counter[0])
 		shifted_output = tf.concat([self.memory[:1], self.output[1:]], axis = 0)
-		shifted_output = tf.roll(self.output, shift = 1, axis = 0)
-		self.output = O * shifted_output + (1 - O) * shifted_output
+		shifted_output = tf.roll(shifted_output, shift = 1, axis = 0)
+		self.output = O * shifted_output + (1 - O) * self.output
 
 
 	def program_update(self):
@@ -78,8 +80,8 @@ class Fuzzy_Brainfuck:
 		bacc = 1 - self.direction
 		noz = 1 - self.memory[0, 0]
 		noz_l = 1 - self.loop_counter[0]
-		INC = 1 - (1 - self.direction* self.program[0, LOP] * self.memory[0, 0])*(1 - bacc * self.program[0, LCL] * noz * noz_l)
-		DEC = 1 - (1 - bacc * self.program[0, LOP] * noz)*(1 - self.direction * self.program[0, LCL] * self.memory[0, 0])
+		INC = 1 - (1 - self.direction* self.program[0, LOP] * self.memory[0, 0])*(1 - bacc * self.program[0, LCL] * noz)
+		DEC = 1 - (1 - bacc * self.program[0, LOP] * noz)*(1 - self.direction * self.program[0, LCL] * self.memory[0, 0]*noz_l)
 		self.loop_counter = INC * tf.roll(self.loop_counter, shift = 1, axis = 0) + DEC * tf.roll(self.loop_counter, shift = 1, axis = 0)  + (1-INC-DEC)*self.loop_counter
 		# Reverse the direction upon zeroing after a loop counter decrement
 		REV = DEC * self.loop_counter[0]
