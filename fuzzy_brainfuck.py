bbb3a5fd3f544d75489c7e1289392932022f80cb
Py10lolz <88994127+Py10lolz@users.noreply.github.com>
2024-11-28 14:34:39 +0800

    Update fuzzy_brainfuck.py

--- fuzzy_brainfuck.py
+++ fuzzy_brainfuck.py
@@ -24,9 +24,9 @@ class Fuzzy_Brainfuck:
 			self.raw_program = raw_program
 		self.initialize(inp)
 
-	def initialize(self, inp, program_modified = True):
+	def initialize(self, inp):
 		# PROGRAM
-		if program_modified: self.program = tf.nn.softmax(self.raw_program)
+		self.program = tf.nn.softmax(self.raw_program)
 		self.halt_point = tf.Variable([0.0]*(program_size-1) + [1.0], trainable = False) # marks the very last instruction.
 		self.direction = tf.Variable(1.0, trainable = False) # direction of execution. A value of 1 corresponds to shifting the program forward and a value of 0 corresponds to shifting it backwards.
 		self.halt = tf.Variable(0.0, trainable = False) # tendency to ignore instruction (simulating halting). Value of 1 completely ignores instructions, while a value of 0 allows instructions to be executed in full strength.
